b1a804a99ea46f52e10a1042943b7cbf455a5030
Ghulam Mohiyuddin <subahani98@gmail.com>
2019-04-27 00:54:02 +0530

    Update settings.py

--- first/settings.py
+++ first/settings.py
@@ -86,8 +86,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'nautanki',
-        'USER': 'rizwan',
-        'PASSWORD': 'Rizwan@111',
+        'USER': 'nahiPata',
+        'PASSWORD': '******',
         'HOST': '127.0.0.1',
         'PORT': '3307',
     }
